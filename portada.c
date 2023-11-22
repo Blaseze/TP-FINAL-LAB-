807ff2afeacc63dd3a24f4f5a62ca6a8542dc61c
Agustín Franco <107129354+agusfranco89@users.noreply.github.com>
2023-11-22 16:16:28 -0300

    Add files via upload
    Portada

---
+++ portada.c
@@ -0,0 +1,393 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "gotoxy.h"
+
+void BienvenidaLaboratorioCentral()
+{
+    for (int h =1; h<2; h++)
+    {
+        gotoxy(13,1);
+        color(10);
+        Materia();
+        Sleep(8);
+
+        gotoxy(15,3);
+        color(10);
+        TrabajoPractico();
+        Sleep(8);
+
+        gotoxy(15,5);
+        color(10);
+        Integrantes();
+        Sleep(8);
+
+        gotoxy(19,7);
+        color(3);
+        Agus();
+        Sleep(8);
+
+        gotoxy(15,9);
+        color(3);
+        Gabi();
+        Sleep(8);
+
+        gotoxy(19,11);
+        color(3);
+        Facu();
+        Sleep(8);
+
+        gotoxy(14,13);
+        color(3);
+        Blas();
+        Sleep(2500);
+
+        system("cls");
+
+        gotoxy(50,5);
+        color(14);
+        Hospital();
+        Sleep(8);
+
+        color(15);
+        gotoxy(11,19);
+        printf("Presione una tecla para ingresar.");
+
+
+    }
+    getch();
+}
+void Materia()
+{
+    printf("B");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("v");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("d");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf("s");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("L");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("b");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("t");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("2");
+    Sleep(25);
+}
+
+void TrabajoPractico()
+{
+
+    printf("T");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("b");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("j");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("P");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("c");
+    Sleep(25);
+    printf("t");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("c");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("F");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("l");
+    Sleep(25);
+}
+
+void Hospital()
+{
+
+    printf("L");
+    Sleep(25);
+    printf("A");
+    Sleep(25);
+    printf("B");
+    Sleep(25);
+    printf("O");
+    Sleep(25);
+    printf("R");
+    Sleep(25);
+    printf("A");
+    Sleep(25);
+    printf("T");
+    Sleep(25);
+    printf("O");
+    Sleep(25);
+    printf("R");
+    Sleep(25);
+    printf("I");
+    Sleep(25);
+    printf("O");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("C");
+    Sleep(25);
+    printf("E");
+    Sleep(25);
+    printf("N");
+    Sleep(25);
+    printf("T");
+    Sleep(25);
+    printf("R");
+    Sleep(25);
+    printf("A");
+    Sleep(25);
+    printf("L");
+    Sleep(25);
+
+}
+
+void Integrantes()
+{
+    printf("I");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("t");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("g");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("t");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("s");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("d");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("l");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("q");
+    Sleep(25);
+    printf("u");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("p");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+}
+
+void Agus()
+{
+    printf("F");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("c");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf(",");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("A");
+    Sleep(25);
+    printf("g");
+    Sleep(25);
+    printf("u");
+    Sleep(25);
+    printf("s");
+    Sleep(25);
+    printf("t");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+}
+
+void Gabi()
+{
+    printf("M");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("d");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf(",");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("G");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("b");
+    Sleep(25);
+    printf("r");
+    Sleep(25);
+    printf("i");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("l");
+    Sleep(25);
+}
+
+void Facu()
+{
+    printf("T");
+    Sleep(25);
+    printf("u");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf(",");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("F");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("c");
+    Sleep(25);
+    printf("u");
+    Sleep(25);
+    printf("n");
+    Sleep(25);
+    printf("d");
+    Sleep(25);
+    printf("o");
+    Sleep(25);
+
+
+}
+
+void Blas()
+{
+    printf("V");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("l");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("s");
+    Sleep(25);
+    printf("q");
+    Sleep(25);
+    printf("u");
+    Sleep(25);
+    printf("e");
+    Sleep(25);
+    printf("z");
+    Sleep(25);
+    printf(",");
+    Sleep(25);
+    printf(" ");
+    Sleep(25);
+    printf("B");
+    Sleep(25);
+    printf("l");
+    Sleep(25);
+    printf("a");
+    Sleep(25);
+    printf("s");
+    Sleep(25);
+}
